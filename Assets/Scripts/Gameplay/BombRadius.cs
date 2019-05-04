b46ee86254f6b4183ed840f4f70e85b2c8ea1030
EBolduc <punk54_ca@hotmail.com>
2019-05-03 23:16:32 -0300

    Worm & Collectors fixed
    -New items re-activating correctly after being deactivated by Collectors.
    -Worm has been tested with Collectors and now works correctly.
    -There is an issue with BombRadius.cs that I've noticed, Coroutine for bomb propulsing other items was put inside OnTriggerStay2D and doesn't seem to work correctly.

--- Assets/Scripts/Gameplay/BombRadius.cs
+++ Assets/Scripts/Gameplay/BombRadius.cs
@@ -4,47 +4,34 @@ using UnityEngine;
 
 public class BombRadius : MonoBehaviour
 {
-
     [SerializeField] Rigidbody2D wormBody, scarabeBody, flyBody, bombBody;
     [SerializeField] Animator anim;
     public float powerX = .5f, powerY = .5f;
-
-   
-
-    private void Awake()
-    {
-        
-    }
-
-    private void FixedUpdate()
-    {
-     
-    }
 
     void OnTriggerStay2D(Collider2D target)
     {
         if (anim.GetBool(TagManager.FLAME_TAG) == true)
         {
-
             if (target.tag == TagManager.FOOD_TAG || 
                 target.tag == TagManager.WORM_TAG ||
                 target.tag == TagManager.BOMB_TAG)
             {
                 StartCoroutine (ForceApplied());
-            }
-
-           
-            IEnumerator ForceApplied()
-            {
-                yield return new WaitForSeconds(1f);
-                target.attachedRigidbody.AddRelativeForce(new Vector2(powerX, powerY), ForceMode2D.Impulse);
-            }
+            } 
         }
     }
 
-    
-   
-    
-  
+    IEnumerator ForceApplied()
+    {
+        yield return new WaitForSeconds(1f);
+        wormBody.AddRelativeForce(new Vector2(powerX, powerY), ForceMode2D.Impulse);
+        scarabeBody.AddRelativeForce(new Vector2(powerX, powerY), ForceMode2D.Impulse);
+        flyBody.AddRelativeForce(new Vector2(powerX, powerY), ForceMode2D.Impulse);
+        bombBody.AddRelativeForce(new Vector2(powerX, powerY), ForceMode2D.Impulse);
+    }
+
+
+
+
 
 }//class
--- Assets/Scripts/Gameplay/Collectors/CollectorScript.cs
+++ Assets/Scripts/Gameplay/Collectors/CollectorScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CollectorScript : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D target) {
-        if (target.gameObject != SpawnFood.instance.selectedFood && target.tag == TagManager.FOOD_TAG || target.tag == TagManager.WORM_TAG || target.tag == TagManager.SCARABE_TAG)
+        if (target.gameObject == SpawnFood.instance.selectedFood && target.tag == TagManager.FOOD_TAG || target.tag == TagManager.WORM_TAG || target.tag == TagManager.SCARABE_TAG)
         {
             target.gameObject.SetActive(false);
             StartCoroutine(SpawnDelay());
